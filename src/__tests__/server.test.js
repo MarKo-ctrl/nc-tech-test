7b6c141e2c63cd9c45c66f5a2ebd9b9597424e74
Marios Kontis <kontism@gmail.com>
2022-11-08 10:50:14 +0000

    write tests for GET /cards/:cardId

--- src/__tests__/server.test.js
+++ src/__tests__/server.test.js
@@ -31,3 +31,62 @@ describe('GET /cards', () => {
       })
   });
 });
+
+describe('GET /cards/:cardId', () => {
+  test('200: should respond with a single card', () => {
+    const cardId = 'card003';
+
+    return request(app)
+      .get(`/cards/${cardId}`)
+      .expect(200)
+      .then((card) => {
+        const cardObj = JSON.parse(card.text);
+
+        expect(cardObj).toBeInstanceOf(Object);
+        expect(cardObj).toEqual(
+          expect.objectContaining({
+            title: "card 3 title",
+            imageUrl: "/front-cover-landscape.jpg",
+            card_id: "card003",
+            base_price: 200,
+            availableSizes: [{
+              id: "md",
+              title: "Medium"
+            },
+            {
+              id: "lg",
+              title: "Large"
+            }],
+            pages: [
+              {
+                title: "Front Cover",
+                templateId: "template006"
+              },
+              {
+                title: "Inside Top",
+                templateId: "template007"
+              },
+              {
+                title: "Inside Bottom", 
+                templateId: "template007"
+              },
+              {
+                title: "Back Cover",
+                templateId: "template008"
+              }
+            ]
+          })
+        );
+      });
+  });
+
+  test(`404: responds with an error message when passed a cardId that does not exist`, () => {
+    const cardId = 'card013';
+    return request(app)
+      .get(`/cards/${cardId}`)
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe('Card not found');
+      });
+  });
+});
